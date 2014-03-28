99147f4f162c2bf8d0a5d1f97a6eeb488bce3ce7
gruber86 <mateusz.roman@stxnext.pl>
2014-03-28 18:04:28 +0100

    [#1633] Models

--- stx_presence_analyzer/analyzer/models.py
+++ stx_presence_analyzer/analyzer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class PresenceWeekday(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    avatar = models.CharField(max_length=600)
+        
+class PresenceMeanTime(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    avatar = models.CharField(max_length=600)
+
+class PresenceStart-End(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    avatar = models.CharField(max_length=600)    
